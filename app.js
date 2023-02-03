28f95e089e26705c035b34660a4d85e11594f2d5
77001 <alimzhanzhakashev@gmail.com>
2023-02-03 22:03:55 +0600

    color commit

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-const cols = document.querySelector('.col');
+const cols = document.querySelectorAll('.col');
 
 
 
+function gerenerateRandomColor(){
+    // RBG
+    // #FF0000
+    // #00FF00
+    // #0000FF
+    const hexCodes = '0123456789ABCDEF'
+    let color = ''
+    for (let i = 0; i < 6; i++) {
+        color +=[Math.floor(Math.random() * hexCodes.length) ]
+        
+    }
+    return '#' + color
+}
 
 function setRandomColors(){
     cols.forEach((col) => {
-     col.style.background = 
-    
+        const text = col.querySelector('h2')
+        const color = chroma.random()
+        text.textContent = color
+        col.style.background = color
+    })
 }
 setRandomColors()
+
