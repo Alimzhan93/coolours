b943372c27a3c805846c2baebfcb7dfc57538f43
77001 <alimzhanzhakashev@gmail.com>
2023-02-04 13:20:16 +0600

    click color, space color

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 const cols = document.querySelectorAll('.col');
+const btn = document.querySelector('.btn')
+
+document.addEventListener('keydown', (Event) =>{
+    if(Event.code.toLocaleLowerCase() === 'space'){
+        setRandomColors()
+    
+    }
+})
 
 
+// меняет цвет по клику
+btn.addEventListener('click', () =>{
+    function setRandomColors(){
+        cols.forEach((col) => {
+            const text = col.querySelector('h2')
+            const color = chroma.random()
+            text.textContent = color
+            col.style.background = color
+            btn.style.background = color
+            setTextColor(text, color)
+        })
+    }
+    setRandomColors()
 
+})
+
+//меняет цвета рамдомно
 function gerenerateRandomColor(){
     // RBG
     // #FF0000
@@ -16,13 +40,24 @@ function gerenerateRandomColor(){
     return '#' + color
 }
 
+
 function setRandomColors(){
     cols.forEach((col) => {
         const text = col.querySelector('h2')
+        const button = col.querySelector('button')
         const color = chroma.random()
+        
         text.textContent = color
         col.style.background = color
+
+        setTextColor(text, color)
+        setTextColor(button, color)
     })
 }
 setRandomColors()
+
+function setTextColor(text, color){
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
 
